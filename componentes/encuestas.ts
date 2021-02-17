2e1be2deaf03bdf915249a52689f8f0fb47d29a9
Ezequiel <ezequielucho@gmail.com>
2021-02-17 12:00:37 +0100

    Tags REVISADO y NO CONTESTAR añadidos

--- componentes/encuestas.ts
+++ componentes/encuestas.ts
@@ -25,14 +25,13 @@ function traductorTipoTurno(tipo: string)
         case 'N': return 'Noche';
     }
 }
-async function enviarKeyboardTurnos(arrayTurnos: Array<any>, idTrabajador: number)
+async function enviarKeyboardTurnos(arrayTurnos: Array<any>, idTrabajador: number, idCodigo: string)
 {
     var lineas = [];
-    
+
         for(let i = 0; i < arrayTurnos.length; i++)
         {
-
-            var nuevoId = await conexion.recHit('hit', `INSERT INTO bot_auxiliar (dato1, dato2, dato3) VALUES ('${nombreTablaPlanificacio(getLunes(arrayTurnos[i].fecha))}', '${arrayTurnos[i].idPlan}', ${idTrabajador}); SELECT SCOPE_IDENTITY() as id `);
+            var nuevoId = await conexion.recHit('hit', `INSERT INTO bot_auxiliar (dato1, dato2, dato3, dato4) VALUES ('${nombreTablaPlanificacio(getLunes(arrayTurnos[i].fecha))}', '${arrayTurnos[i].idPlan}', ${idTrabajador}, '${idCodigo}'); SELECT SCOPE_IDENTITY() as id `);
                 lineas.push([{text: `Turno: ${arrayTurnos[i].nombreTienda} - ${traductorTipoTurno(arrayTurnos[i].tipoTurno)} - ${arrayTurnos[i].fecha.toLocaleString()}`, callback_data: JSON.stringify({acc: 'updTurno', id: nuevoId.recordset[0].id})}]);
         }
         lineas.push([{text: `Crear turno nuevo`, callback_data: JSON.stringify({})}]);
@@ -49,27 +48,36 @@ bot.on('message', function(msg) {
         mainBot(msg, false).then(sesion=>{
             if(!sesion.error)
             {
-                let sqlCodisAccio = "select top 1 Param1 as idTienda, DAY(CONVERT(datetime, Param3, 105)) as dia, MONTH(CONVERT(datetime, Param3, 105)) as mes, YEAR(CONVERT(datetime, Param3, 105)) as year, Param3 as entrada, Param4 as salida, (select nom from Dependentes where codi = Param2) as nombreTrabajador, Param2 as idTrabajador from CodigosDeAccion where tipoCodigo = 'FICHAJE' order by TmStmp desc";
+                let sqlCodisAccio = "select top 1 IdCodigo as idCodigo, Param1 as idTienda, DAY(CONVERT(datetime, Param3, 105)) as dia, MONTH(CONVERT(datetime, Param3, 105)) as mes, YEAR(CONVERT(datetime, Param3, 105)) as year, Param3 as entrada, Param4 as salida, (select nom from Dependentes where codi = Param2) as nombreTrabajador, Param2 as idTrabajador from CodigosDeAccion where tipoCodigo = 'FICHAJE' AND Param9 IS NULL order by TmStmp desc";
 
-                conexion.recHit(sesion.database, sqlCodisAccio).then(res=>{
-                    var sql = '';
-                    for(let i = 0; i < res.recordset.length; i++)
+                conexion.recHit(sesion.database, sqlCodisAccio).then(async res=>{
+                    
+                    await conexion.recHit(sesion.database, "UPDATE CodigosDeAccion SET Param9 = 'NO CONTESTAR' WHERE idCodigo = '" + res.recordset[0].idCodigo + "'");
+                    if(res.recordset.length > 0)
                     {
-                        let fecha = new Date(res.recordset[i].year, Number(res.recordset[i].mes)-1, res.recordset[i].dia);
-
-                        if(i == res.recordset.length - 1) //Es el último
+                        var sql = '';
+                        for(let i = 0; i < res.recordset.length; i++)
                         {
-                            sql += `select botiga as idTienda, (select nom from clients where codi = botiga) as nombreTienda, periode as tipoTurno, fecha, idPlan, idTurno from ${nombreTablaPlanificacio(getLunes(fecha))} where day(fecha) = ${res.recordset[i].dia} AND month(fecha) = ${res.recordset[i].mes} AND year(fecha) = ${res.recordset[i].year} AND activo = 1 AND idEmpleado IS NULL AND idTurno IS NOT NULL AND BOTIGA = ${res.recordset[i].idTienda}`;
+                            let fecha = new Date(res.recordset[i].year, Number(res.recordset[i].mes)-1, res.recordset[i].dia);
+    
+                            if(i == res.recordset.length - 1) //Es el último
+                            {
+                                sql += `select botiga as idTienda, (select nom from clients where codi = botiga) as nombreTienda, periode as tipoTurno, fecha, idPlan, idTurno from ${nombreTablaPlanificacio(getLunes(fecha))} where day(fecha) = ${res.recordset[i].dia} AND month(fecha) = ${res.recordset[i].mes} AND year(fecha) = ${res.recordset[i].year} AND activo = 1 AND idEmpleado IS NULL AND idTurno IS NOT NULL AND BOTIGA = ${res.recordset[i].idTienda}`;
+                            }
+                            else
+                            {
+                                sql += `select botiga as idTienda, (select nom from clients where codi = botiga) as nombreTienda, periode as tipoTurno, fecha, idPlan, idTurno from ${nombreTablaPlanificacio(getLunes(fecha))} where day(fecha) = ${res.recordset[i].dia} AND month(fecha) = ${res.recordset[i].mes} AND year(fecha) = ${res.recordset[i].year} AND activo = 1 AND idEmpleado IS NULL AND idTurno IS NOT NULL AND BOTIGA = ${res.recordset[i].idTienda} UNION `;
+                            }
                         }
-                        else
-                        {
-                            sql += `select botiga as idTienda, (select nom from clients where codi = botiga) as nombreTienda, periode as tipoTurno, fecha, idPlan, idTurno from ${nombreTablaPlanificacio(getLunes(fecha))} where day(fecha) = ${res.recordset[i].dia} AND month(fecha) = ${res.recordset[i].mes} AND year(fecha) = ${res.recordset[i].year} AND activo = 1 AND idEmpleado IS NULL AND idTurno IS NOT NULL AND BOTIGA = ${res.recordset[i].idTienda} UNION `;
-                        }
+                        conexion.recHit(sesion.database, sql).then(async resTurnos=>{
+    
+                            bot.sendMessage(msg.chat.id, `${res.recordset[0].nombreTrabajador}. \nEntrada: ${res.recordset[0].entrada}\nSalida: ${res.recordset[0].salida} `, await enviarKeyboardTurnos(resTurnos.recordset, res.recordset[0].idTrabajador, res.recordset[0].idCodigo));
+                        });  
                     }
-                    conexion.recHit(sesion.database, sql).then(async resTurnos=>{
-
-                        bot.sendMessage(msg.chat.id, `${res.recordset[0].nombreTrabajador}. \nEntrada: ${res.recordset[0].entrada}\nSalida: ${res.recordset[0].salida} `, await enviarKeyboardTurnos(resTurnos.recordset, res.recordset[0].idTrabajador));
-                    });                   
+                    else
+                    {
+                        bot.sendMessage(msg.chat.id, 'No hay fichajes pendientes de revisión');
+                    }
                 });              
             }
         });
@@ -84,8 +92,10 @@ bot.on('callback_query', async (msg) => {
             if(!sesion.error)
             {
                 let chatId = msg.message.chat.id;
-                conexion.recHit('hit', 'select dato1 as nombreTabla, dato2 as idPlan, dato3 as idTrabajador from bot_auxiliar where id = ' + callbackData.id).then(res=>{
-                    conexion.recHit(sesion.database, `UPDATE ${res.recordset[0].nombreTabla} SET idEmpleado = ${res.recordset[0].idTrabajador} WHERE idPlan = '${res.recordset[0].idPlan}'`).then(ok=>{
+                conexion.recHit('hit', 'select dato1 as nombreTabla, dato2 as idPlan, dato3 as idTrabajador, dato4 as idCodigo from bot_auxiliar where id = ' + callbackData.id).then(res=>{
+                    conexion.recHit(sesion.database, `UPDATE ${res.recordset[0].nombreTabla} SET idEmpleado = ${res.recordset[0].idTrabajador} WHERE idPlan = '${res.recordset[0].idPlan}'
+                                                      UPDATE CodigosDeAccion SET Param9 = 'REVISADO' WHERE idCodigo = '${res.recordset[0].idCodigo}';
+                                                        `).then(ok=>{
                         bot.sendMessage(chatId, 'Fichaje asignado correctamente');
                     });
                 });
